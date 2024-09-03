bbfc3ccf52a5074f4ef228671de3ce0dc791ab23
Egor Fortov <egorfortov@gmail.com>
2024-09-03 23:44:43 +0300

    data models done

---
+++ src/main/java/ru/yandex/practicum/catsgram/model/User.java
@@ -0,0 +1,18 @@
+package ru.yandex.practicum.catsgram.model;
+
+import lombok.Data;
+
+import java.time.Instant;
+
+@Data
+public class User {
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    private Instant registrationDate;
+
+    // Геттеры, сеттеры, методы toString, equals и hashCode
+}
+
+
